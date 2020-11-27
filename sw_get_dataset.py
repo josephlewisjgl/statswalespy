bc33ec5de2b6ca11302fbb37ae1831b4d157217b
josephlewisjgl <joe@Joes-MacBook.local>
2020-11-27 13:33:52 +0000

    Add basic functionality to extract dataset from StatsWales as a dataframe

---
+++ sw_get_dataset.py
@@ -0,0 +1,43 @@
+"""
+
+StatsWalesPy will be a package that takes data from StatsWales into Python.
+This file contains the function to get this data using the OData feed from a known cube.
+
+"""
+import requests
+import pandas as pd
+
+
+def sw_get_dataset(id, print_progress=False):
+    # Define dataset URL --------------------------------------------------------
+    url = "http://open.statswales.gov.wales/en-gb/dataset/" + id
+
+    # Extract first page and add dataframe to list ------------------------------
+    json_data = requests.get(url).json()
+
+    # Add this set of json_data to a list TODO: Error handle the no data
+
+    json_list = [json_data['value']]
+
+    # Loop through odata links to get all data --------------------------------
+    i = 0
+
+    while "odata.nextLink" in json_data:
+
+        # print progress message
+        if print_progress:
+            i = i + len(json_data['value'])
+            print("Extracting data " + str(i) + " rows extracted")
+
+        json_data = requests.get(json_data["odata.nextLink"]).json()
+
+        json_list.append(json_data['value'])
+
+    # bind dataframes together and tell user the data is extracted
+    json_flat = [l for json in json_list for l in json]
+    df = pd.DataFrame(json_flat)
+
+    print("Data extracted with " + str(len(df)) + " rows, and " + str(len(df.columns)) + " columns.")
+
+    # Return the dataframe
+    return df
